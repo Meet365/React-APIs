fe66d9f43778e96678d33d0747ba51934be0d70f
MEET <dangimeet1103@gmail.com>
2024-09-05 18:10:18 +0530

    creating some modal group with react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,38 +1,79 @@
 import "./App.css";
 import Employee from "./components/Employee";
+
 import { useState } from "react";
+import {v4 as uuidv4} from 'uuid';
 
 function App() {
   console.log("we r about to list the employee");
   let showEmployee = true;
 
   const [role, setRole] = useState("dev");
+  const [employee, setEmployee] = useState([
+    {
+      name: "meet",
+      role: "head-Developer",
+      img: "https://images.pexels.com/photos/7688086/pexels-photo-7688086.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      name: "aby",
+      role: "senior-Developer",
+      img: "https://images.pexels.com/photos/7552372/pexels-photo-7552372.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      name: "amy",
+      role: "junior-Developer",
+      img: "https://images.pexels.com/photos/12903143/pexels-photo-12903143.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      name: "mayene",
+      role: "intern-Developer",
+      img: "https://images.pexels.com/photos/12903191/pexels-photo-12903191.jpeg",
+    },
+    {
+      name: "jina",
+      role: "Designer",
+      img: "https://images.pexels.com/photos/7792859/pexels-photo-7792859.jpeg",
+    },
+    {
+      name: "lady zhen",
+      role: "sales-Assistant",
+      img: "https://images.pexels.com/photos/7688444/pexels-photo-7688444.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      name: "da-xiao",
+      role: "HR",
+      img: "https://images.pexels.com/photos/7688444/pexels-photo-7688444.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+  ]);
 
   return (
     <>
-    <div className="App ">
-      <input
-        type="text"
-        onChange={(e) => {
-          console.log(e.target.value);
-          setRole(e.target.value);
-        }}
-      ></input>
-      {showEmployee ? 
-      <>
-      <div className="flex flex-wrap justify-center">
-
-     
-      <Employee name="meet" role={role} img="https://images.pexels.com/photos/7688444/pexels-photo-7688444.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/> 
-      <Employee name="aby" role={role} img="https://images.pexels.com/photos/7993953/pexels-photo-7993953.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/> 
-      <Employee name="met" role={role} img="https://images.pexels.com/photos/7688444/pexels-photo-7688444.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/> 
-      <Employee name="andy" role={role} img="https://images.pexels.com/photos/7993953/pexels-photo-7993953.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/> 
-      <Employee name="dina" role={role} img="https://images.pexels.com/photos/7688444/pexels-photo-7688444.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/> 
-      <Employee name="jina" role={role} img="https://images.pexels.com/photos/7993953/pexels-photo-7993953.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/> 
+      <div className="App ">
+        <input
+          type="text"
+          onChange={(e) => {
+            console.log(e.target.value);
+            setRole(e.target.value);
+          }}
+        ></input>
+        {showEmployee ? (
+          <>
+            <div className="flex flex-wrap justify-center">
+              {employee.map((employee) => {
+                return (
+                  <Employee
+                    key={uuidv4()}
+                    name={employee.name}
+                    role={employee.role}
+                    img={employee.img}
+                  />
+                );
+              })}
+            </div>
+          </>
+        ) : null}
       </div>
-      </>
-      : null}
-    </div>
     </>
   );
 }
