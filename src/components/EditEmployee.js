dc2696e0ceed50f1c2897b29e2b390bd6393f10c
MEET <dangimeet1103@gmail.com>
2024-09-06 15:16:51 +0530

    new change

--- src/components/EditEmployee.js
+++ src/components/EditEmployee.js
@@ -45,7 +45,7 @@ function EditEmployee(props) {
                   className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
                   for="name"
                 >
-                  Full Name
+                  Full Name - or your name 
                 </label>
               </div>
               <div className="md:w-2/3">
