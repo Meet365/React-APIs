67f021dae052058b3557e8accf874e5c48132a44
MEET <dangimeet1103@gmail.com>
2024-09-10 11:53:07 +0530

    made the navbar somewhat working

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import Employees from "./pages/Employees";
 import NavBar from "./components/NavBar";
+import Customers from "./pages/Customers";
+import  {BrowserRouter , Routes, Route} from 'react-router-dom';
+
 
 function App() {
   return (
     <>
       <NavBar>
-        <Employees />
+        <BrowserRouter>
+        <Routes>
+          <Route path='/employee' element= {<Employees/>} />
+          <Route path='/customers' element= {<Customers/>} />
+        </Routes>
+        </BrowserRouter>
       </NavBar>
     </>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,8 +10,8 @@ import {
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 
 const navigation = [
-  { name: "Dashboard", href: "#", current: true },
-  { name: "Team", href: "#", current: false },
+  { name: "Employees", href: "/employee", current: true },
+  { name: "Customers", href: "/customers", current: false },
   { name: "Projects", href: "#", current: false },
   { name: "Calendar", href: "#", current: false },
 ];
